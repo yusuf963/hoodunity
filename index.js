f16017bdf8e49b99903032703ef49808cbc12a23
yusuf963 <mohammad963yusuf@gmail.com>
2021-02-28 19:58:06 +0000

    adding router to app.js

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 import express from 'express'
 import connectToDatabase from './config/connectToDB.js'
-
+import router from './views/router.js'
 
 const app = express()
-
 const startServer = async () => {
-  // await connectToDatabase()
+  await connectToDatabase()
+  console.log("connectToDatabase")
   app.use(express.json())
 
-  app.use('/', (req, res, next) => {
-    res.status(200).send('<h1>welcome to Home Page</h1>')
+  app.use('/api', router)
+
+  const port = process.env.PORT || 8000
+  app.listen(port, () => {
+    console.log(`listening on port, ${port}`)
   })
 }
-
-const port = process.env.PORT || 8000
-app.listen(port, () => {
-  console.log(`listening on port, ${port}`)
-})
 
 startServer()
